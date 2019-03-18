3fa5f277a6c534c5142478ca5aea2bf85e57dfb1
sunny <jjohye11@gmail.com>
2019-03-18 10:55:37 +0900

    네트워크 문제풀이 수정

--- ALGORITHM/네트워크.cpp
+++ ALGORITHM/네트워크.cpp
@@ -2,55 +2,38 @@
 * https://programmers.co.kr/learn/courses/30/lessons/43162
 *
 * 네트워크
-* 
-** 특이사항 . 아래 코드는 테스트에 통과하지 못한 코드임
-*
-* 테스트 1 〉	통과 (0.03ms, 3.78MB)
-* 테스트 2 〉	통과 (0.06ms, 3.95MB)
-* 테스트 3 〉	실패 (0.06ms, 3.83MB)
-* 테스트 4 〉	실패 (0.06ms, 3.93MB)
-* 테스트 5 〉	통과 (0.02ms, 3.77MB)
-* 테스트 6 〉	통과 (0.05ms, 3.94MB)
-* 테스트 7 〉	실패 (0.06ms, 3.78MB)
-* 테스트 8 〉	실패 (0.07ms, 3.85MB)
-* 테스트 9 〉	실패 (0.07ms, 3.88MB)
-* 테스트 10 〉	실패 (0.09ms, 3.86MB)
-* 테스트 11 〉	통과 (0.07ms, 3.95MB)
-* 테스트 12 〉	통과 (0.07ms, 3.96MB)
-* 테스트 13 〉	실패 (0.08ms, 3.91MB)
 *
 * @ddunny
 */
 #include <string>
 #include <vector>
-#include <iostream>
 
 using namespace std;
-int visit[200][200] = { 0 , };
+int visit[200] = { 0 , };
+vector<vector<int>>  copy_com;
 
-int solution(int n, vector<vector<int>> computers) {
-    int network = 0;
-    network = computers.size();
+void dfs(vector<int> computers) {
     for (int i = 0; i < computers.size() ; i++) {
-        for (int j = 0; j < computers.size() ; j++) {
-             if (i == j) { visit[i][j] = 1; }
-            if (visit[i][j]) { //이미 점검이 완료된 곳이라면 더이상 탐색하지 않는다.
-                break;
-            }
-            
-            if (computers.at(i).at(j)) { //연결된 곳이라면 
-                visit[i][j] = 1;
-                visit[j][i] = 1;
-                network -= 1; // 네트워크가 연결된 것이므로 1개를 감소시킨다.
-                if (network == 1) { // 이미 다 하나로 연결되었으므로 더이상 돌지 않는다.
-                    break;
-                }
-            }
-        }
-        if (network == 1) { // 이미 다 하나로 연결되었으므로 더이상 돌지 않는다.
-            break;
+        if (visit[i]) //이미 이전에 탐색을 했던 곳이라면 연결 탐색하지 않는다.
+            continue;
+        if (computers.at(i)) { //네트워크 연결된 것
+            //  다음 연결된 것을 찾아본다.
+            visit[i] = true;
+            dfs(copy_com.at(i));
         }
     }
-    cout << "network: " << network << endl;
-    return network;
+}
+
+int solution(int n, vector<vector<int>> computers) {
+    int answer = 0;
+    copy_com = computers;
+    for (int i = 0; i < computers.size() ; i++) {
+        if (!visit[i]) {
+            visit[i] = true;
+            dfs(computers.at(i)); //연결된 컴퓨터로 이동하여, 또 다른 연결된 컴퓨터가 있는지 확인해봐야 한다.
+            answer++; //연결 확인하는 작업이 끝났으므로, 하나의 네크워크가 생성된 것으로 확인한다.
+            // 연결을 확인하는 작업: 연결여부를 떠나서 무조건 컴퓨터를 탐색하므로, 커넥션이 없어도 answer은 카운팅 +1 된다.
+        }
+    }
+    return answer;
 }
