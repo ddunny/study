9991a47e3ae877890479e8db32dd641dd7e5884d
sunny <jjohye11@gmail.com>
2019-04-06 00:53:25 +0900

    무한루프 수정
    코드결과
    - 틀렸습니다.

--- ALGORITHM/발전소.cpp
+++ ALGORITHM/발전소.cpp
@@ -34,8 +34,9 @@ int main() {
 		for (int j = 0; j < N; j++) {
 			cin >> cost[i][j]; //발전소 i를 이용해서 발전소 j를 재시작할 때 드는 비용
 		}
+		memo[i] = 51; // 비용은 50보다 작거나 같은 음이 아닌 정수이므로, 최소값을 구해야 하므로, 나올 수 없는 최대값으로 초기설정을 해준다.
 	}
-	char input; 
+	char input;
 	for (int i = 0; i < N; i++) {  // 발전소가 켜져있으면 Y, 꺼져있으면 N
 		cin >> input;
 		if (input == 'Y') {
@@ -50,42 +51,35 @@ int main() {
 
 	cin >> P;  //적어도 P개의 발전소가 고장나 있지 않도록   // 0 <= P < N
 
+	if (good.size() == 0) { //모두 고장난 경우에는 발전소를 고칠 수 없다.
+		cout << "-1" << endl;
+		return 0;
+	}
+
 	while (!bad.empty()) { //다 고쳐질 때까지 
 		int needfix = bad.front();
 		bool fixed = false;
-		cout << "need fix: " << needfix << endl;
 		for (int i = 0, max = good.size(); i < max; i++) {
 			int idx = good[i];
-			if (cost[idx][needfix] <= 0) { // 0이상이어야 비용이 드는 것이다
+			if (cost[idx][needfix] == 0) { // 기기 자신을 의미함
 				continue;
 			}
-			if (memo[needfix] == 0) {
+			if (cost[idx][needfix] < memo[needfix]) {
 				memo[needfix] = cost[idx][needfix];
-				
-				if (i + 1 == max) { //마지막인 경우
-					good.push_back(needfix); //고쳐진거 집어 넣기
-					fixed = true;
-					break;
-				}
-			}
-			if (memo[needfix] > cost[idx][needfix]) {
-				memo[needfix] = cost[idx][needfix];
-				fixed = true;
-
 			}
 		}
-
-		if (fixed == true) {
-			fixed = false;
-			bad.pop();
-		}
+		bad.pop();
+		good.push_back(needfix);
 	}
 
 	int answer = 0;
 	for (int i = 0; i < N; i++) {
+		if (memo[i] == 0 || memo[i] == 51) {
+			continue;
+		}
 		answer += memo[i];
 	}
-	
+
 	cout << answer; //첫째 줄에 문제의 정답을 출력한다. 불가능한 경우에는 -1을 출력한다.
 
 	return 0;
