33e2a0cb33fdd88095b33dc221ed7571bebc4d28
ddunny <44994243+ddunny@users.noreply.github.com>
2019-05-27 22:51:47 +0900

    코드 잘못된 부분 수정

--- ALGORITHM/bj4963.cpp
+++ ALGORITHM/bj4963.cpp
@@ -5,6 +5,10 @@
 *
 * 고려해야 할 사항
 * 주어진 테스트 케이스, 질문 검색 내 일부 테스트케이스들 테스트 해봤으나 모두 정담... 왜 틀린지 모르겠음.....
+*
+* 틀렸던 이유
+* 1. dir 1개를 누락하고, 중복으로 두었음
+* 2. dfs 들어가는 값의 x, y 좌표를 반대로 함
 * @ddunny
 */
 #include <iostream>
@@ -17,12 +21,11 @@ using namespace std;
 #define VISIT 0
 
 int w, h;
-int map[55][55];
-int dir[8][2] = { {0,1}, {1,0}, {1,1},{-1, 1},{-1 ,0}, {-1, 1}, {-1,-1}, {0, -1} };
+int map[51][51];
+int dir[8][2] = { {0, 1}, {1, 0}, {1, 1},{-1, 1},{-1 ,0}, {1, -1}, {-1,-1}, {0, -1} };
 int result = 0;
 
-struct position
-{
+struct position {
 	int y, x;
 };
 
@@ -41,26 +44,19 @@ void dfs(position stt) { // 섬의 개수 세기
 		for (int i = 0; i < 8; i++) {
 			next.y = now.y + dir[i][0];
 			next.x = now.x + dir[i][1];
-			//cout << "now map[" << now.y << "][" << now.x << "] : " << map[now.y][now.x] << endl;
-			//cout << "next map[" << next.y << "][" << next.x << "] : " << map[next.y][next.x] << endl;
 			if (map[next.y][next.x] == LAND && next.y < h && next.x < w && 0 <= next.y && 0 <= next.x) {
-				//cout << "next is land" << endl;
 				map[next.y][next.x] = VISIT;
 				stacks.push(next);
 			}
 		}
-		//cout << "======" << endl;
 	}
-	result++;
-	//cout << "in DFS result is " << result << endl;
+	result++; // 스택을 다 비우고 카운팅 -> 이어져 있는 곳 체킹이 끝난 것.
 }
 
 int main() {
-	//int count = 0;
 	while (1) {
-		memset(map, 0, sizeof(map));
 		cin >> w >> h;
-		if (w == 0 && h == 0)
+		if (w == 0 && h == 0) // 0, 0 이 들어오면 입력 끝내기 
 			break;
 
 		for (int i = 0; i < h; i++) {
@@ -71,17 +67,15 @@ int main() {
 
 		for (int i = 0; i < h; i++) {
 			for (int j = 0; j < w; j++) {
-				if (map[j][i] == LAND) {
-					map[j][i] = VISIT;
-					//cout << "j is " << j << " i is " << i << endl;
-					dfs({ j, i });
+				if (map[i][j] == LAND) {
+					map[i][j] = VISIT;
+					dfs({ i, j });
 				}
 			}
 		}
 		cout << result << endl;
-		//cout << "result: " << result << endl;
-		result = 0;
-		//cout << "turn count: " << ++count << "==========================" << endl;
+		memset(map, 0, sizeof(map)); // 지도 0으로 초기화
+		result = 0; //초기화 
 	}
 	return 0;
 }
