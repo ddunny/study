4c93aa62ac4b1fd1673c70d21564936e0438acd1
sunny <jjohye11@gmail.com>
2019-08-06 08:47:19 +0900

    solve 가장 큰 수

--- ALGORITHM/js/programmers-42746.js
+++ ALGORITHM/js/programmers-42746.js
@@ -1,22 +1,34 @@
+/**
+ * 가장 큰 수
+ * https://programmers.co.kr/learn/courses/30/lessons/42746
+ *
+ * 
+ @ddunny
+ */
 function solution(numbers) {
-    var answer = '';
-    
-    let temp = numbers.slice(0);
-    
-    temp.sort((a,b)=> {
-        const charA = a.toString().charAt(0);
-        const charB = b.toString().charAt(0);
-        if (charA == charB) {
-            return a < b;
-        }
-        return charA < charB;
-    }); // 첫번째 자리 기준으로 정렬
-    
-    console.log(`temp ${temp}`);
-    
-    answer = temp.join("").toString(); // 정답이 너무 클 수 있으니 문자열로 바꾸어 return
-    return answer;
+  let answer = '';
+  let temp = numbers.slice(0);
+  temp.sort((a, b) => {
+    const charA = a.toString();
+    const charB = b.toString();
+    return parseInt(charB + charA) - parseInt(charA + charB);
+  }); // 첫번째 자리 기준으로 정렬
+
+  if (temp[0] === 0) {
+    return "0";
+  }
+
+  answer = temp.join(""); // 정답이 너무 클 수 있으니 문자열로 바꾸어 return
+  return answer;
 }
 
-// 첫번째 풀이
-// [3, 30, 34, 5, 9]	9534330 통과 못함
+/**
+ * 다른 사람의 풀이
+ * function solution(numbers) {
+ *   let temp = numbers.map(v => v + "")
+ *     .sort((a, b) => parseInt(b + a) - parseInt(a + b))
+ *     .join("");
+ * 
+ *   return (temp[0] === "0") ? "0" : temp;
+}
+ */
