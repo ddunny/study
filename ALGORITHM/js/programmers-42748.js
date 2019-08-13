538710eb057a11e1419dfbd1a6ed55c116107af4
sunny <jjohye11@gmail.com>
2019-08-13 09:40:50 +0900

    solve K번째 수 리팩토링

--- ALGORITHM/js/programmers-42748.js
+++ ALGORITHM/js/programmers-42748.js
@@ -6,24 +6,14 @@
  @ddunny
  */
 function solution(array, commands) {
-    let answer = [];
-    let item = [];
-    for (let idx in commands) {
-        item = array
-            .filter((v, x)=>{
-                if ( commands[idx][0] <= x + 1 && x + 1 <= commands[idx][1]) {
-                    return v;
-                }
-            })
-            .sort((a, b)=> a - b)
-            .filter((v, x)=> {
-            if(commands[idx][2] === x + 1){
-                return v;
-            }
-        });
-        
-        answer.push(item.join("")*1);
-    }
-    
-    return answer;
+  let answer = [];
+  for (let idx in commands) {
+    answer[idx] = array
+                  .filter((v, x) => commands[idx][0] <= x + 1 && x + 1 <= commands[idx][1])
+                  .sort((a, b) => a - b)
+                  .filter((v, x) => commands[idx][2] === x + 1)
+                  .join("") * 1;
+  }
+
+  return answer;
 }
