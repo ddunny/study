c05bfb397049b49d22ada755bffd4de1bf119714
sunny <jjohye11@gmail.com>
2019-08-21 08:44:22 +0900

    불필요한 변수 삭제

--- ALGORITHM/js/icecream-parlor.js
+++ ALGORITHM/js/icecream-parlor.js
@@ -9,17 +9,11 @@
    // m : 둘이 사용할 수 있는 최대 비용
    // arr : 아이스크림의 가격
    // 규칙: 둘은 같은 제품을 중복해서 사지 않는다.
-
-   let sunny = 0;
-   let johnny = 0;
-
-   for (let i = 0; i < arr.length; i++) {
-     sunny = i;
-     for (let j = 0; j <= arr.length; j++) {
-       if (arr[sunny] + arr[j] === m && i !== j) {
-         johnny = j;
-         return [sunny + 1, johnny + 1];
-       }
-     }
-   }
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j <= arr.length; j++) {
+        if (arr[i] + arr[j] === m && i !== j) {
+          return [i + 1, j + 1];
+        }
+      }
+    }
  }
