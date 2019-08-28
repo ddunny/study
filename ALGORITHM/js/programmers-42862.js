45d4b77ece37eac56775b0b02ae9e57b9855d5cf
sunny <jjohye11@gmail.com>
2019-08-29 08:57:08 +0900

    체육복 수정

--- ALGORITHM/js/programmers-42862.js
+++ ALGORITHM/js/programmers-42862.js
@@ -3,6 +3,8 @@
  * https://programmers.co.kr/learn/courses/30/lessons/42862
  *
  * - 테스트 케이스 12개중 마지막 케이스 통과 못함
+ * - 통과 못했던 이유 : 중복을 먼저 제거 해야 함 
+ *   - 참고 : https://programmers.co.kr/learn/questions/5434
  @ddunny
  */
 
@@ -13,19 +15,20 @@ function solution(n, lost, reserve) {
 
     let answer = n - lost.length; // 확실하게 수업을 들을 수 있는 학생의 수
 
-    // 여벌을 가져온 번호의 학생이 도난당한 리스트에 있는지 확인
-    // 여벌이 가져온 학생이 옷을 도난당했을 경우 //reseve 에서 제거하기 
+    // 여벌을 가져온 학생이 옷을 도난당한 경우 처리
+    for (let i = 0; i < lost.length; i++) {
+        let w = reserve.indexOf(lost[i]);
+        if (w !== -1) {
+            lost[i] = -99;
+            reserve[w] = -99;
+            answer++; // 운동할 수 있으므로 ++
+        }
+    }
 
-    for (let idx in lost) { // 운동할 수 있는지 처리해야 하니까 이거 기준으로 한다
+    for (let idx in lost) { // 운동할 수 있는지 처리해야 하니까 lost 기준으로 loop
+        if (lost[idx] === -99) continue;
         let pre = reserve.indexOf(lost[idx] - 1);
         let next = reserve.indexOf(lost[idx] + 1);
-        let now = reserve.indexOf(lost[idx]);
-
-        if (now !== -1) { // 여벌도 있고, 도난도 당했을 경우
-            reserve[now] = -99;
-            answer++;
-            continue;
-        }
 
         if (pre !== -1 || next !== -1) {
             if (pre !== -1) {
@@ -33,7 +36,7 @@ function solution(n, lost, reserve) {
             } else {
                 reserve[next] = -99;
             }
-            answer++;
+            answer++; // 운동할 수 있으므로 ++
         }
     }
     return answer;
