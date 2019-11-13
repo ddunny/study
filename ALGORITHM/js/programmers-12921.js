34fe51201c8d82daa116ed81e50baeb6d907fc31
sunny <jjohye11@gmail.com>
2019-11-14 08:46:04 +0900

    소수 찾기 문제풀이 - 2번째
    - 첫번째 풀이 개선
      - answer를 -- 해나가는 식으로 변경하여, 답을 구하기 위한 별도의 loop문이 필요하지 않음

--- ALGORITHM/js/programmers-12921.js
+++ ALGORITHM/js/programmers-12921.js
@@ -4,15 +4,17 @@
  * 소수 찾기
  * https://programmers.co.kr/learn/courses/30/lessons/12921
  * 
- * - 가장 시간이 많이 걸렸던 케이스
- *   - 정확성 : 테스트 11 (102.84ms, 56.1MB)
- *   - 효율성: 테스트 3 (106.88 ms, 56.6 MB)
- *
+ * - 첫번째 풀이 개선
+ *   - answer를 -- 해나가는 식으로 변경하여, 답을 구하기 위한 별도의 loop문이 필요하지 않음
+ *   - 가장 시간이 많이 걸렸던 케이스
+ *     - 정확성 : 테스트 11 (90.33ms, 55.9MB)
+ *     - 효율성 : 테스트 3 (96.93ms, 56.5MB)
  *
  @ddunny
  */
 function solution(n) {
-  let answer = 0;
+  // let answer = 0;
+  let answer = n - 1;
   let arr = [];
   let size = n;
   let target = Math.sqrt(n) // n의 제곱근
@@ -24,14 +26,15 @@ function solution(n) {
     for (let j = i * 2; j <= n; j += i) {
       if (!arr[j - 1]) {
         arr[j - 1] = true;
+        answer--;
       };
       // 배열의 index와 실제 위치의 차이가 1이기 때문에 자리를 확인할 때는 -1을 처리한 위치의 값을 확인합니다.
     }
   }
 
-  for (let i = 1; i < arr.length; i++) { // true로 바뀌지 않은 곳의 값은 소수입니다.
-    if (!arr[i]) answer++;
-  }
+  // for(let i = 1; i < arr.length; i++) {  // true로 바뀌지 않은 곳의 값은 소수입니다.
+  //     if(!arr[i]) answer++; 
+  // }
 
   return answer;
 }
