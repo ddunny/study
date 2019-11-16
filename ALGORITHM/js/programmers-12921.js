d28cddfa0cd7f142ff210a24088d315acd65453c
ddunny <44994243+ddunny@users.noreply.github.com>
2019-11-17 08:07:37 +0900

    소수 찾기 문제풀이 개선

--- ALGORITHM/js/programmers-12921.js
+++ ALGORITHM/js/programmers-12921.js
@@ -9,13 +9,17 @@
  *   - 가장 시간이 많이 걸렸던 케이스
  *     - 정확성 : 테스트 11 (90.33ms, 55.9MB)
  *     - 효율성 : 테스트 3 (96.93ms, 56.5MB)
+ * - 두번째 풀이 개선
+ *   - 배열 생성방법으로 []를 크기를 지정한 new Array() 로 변경함
+ *     - 정확성 : 테스트 11 (42.80ms, 44.1MB)
+ *     - 효율성 : 테스트 3 (48.39ms, 44.5MB)
  *
  @ddunny
  */
 function solution(n) {
-  // let answer = 0;
   let answer = n - 1;
-  let arr = [];
+  // let arr = [];
+  let arr = new Array(n);
   let size = n;
   let target = Math.sqrt(n) // n의 제곱근
 
