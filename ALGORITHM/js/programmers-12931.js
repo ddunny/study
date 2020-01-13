37e7a48d24b44690fbc18b908ff832da636bee1c
sunny <jjohye11@gmail.com>
2020-01-14 08:47:57 +0900

    자릿수 더하기 문제풀이

---
+++ ALGORITHM/js/programmers-12931.js
@@ -0,0 +1,11 @@
+  
+/** 
+ * 자릿수 더하기
+ * https://programmers.co.kr/learn/courses/30/lessons/12931
+ * 
+ * @ddunny
+*/
+function solution(n) {
+    n = n + '';
+    return n.split('').reduce((acc, cur) => acc + cur * 1, 0);
+}
