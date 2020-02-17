b80e584e607c0d79ff9bb8bc4a1731d5e98daf4d
sunny <jjohye11@gmail.com>
2020-02-18 00:20:25 +0900

    올바른 괄호 재풀이
    stack을 이용해야 모든 케이스를 통과할 수 있음

--- ALGORITHM/js/programmers-12909.js
+++ ALGORITHM/js/programmers-12909.js
@@ -1,18 +1,22 @@
 /** 
  * 올바른 괄호
  * https://programmers.co.kr/learn/courses/30/lessons/12909
- * 특이사항
- * 1. 통과하지 못한 소스
- * - 통과하지 못한 케이스 : ())(()
+ * stack을 이용해야 모든 케이스를 통과할 수 있음
  * @ddunny
 */
 
 function solution(s){
     const length = s.length;
-    if (s.charAt(0) === '(' && s.charAt(length - 1) === ')') {
-        let count = 0;
-        s.split('').map(element => element === '(' ? count++ : count--);
-        return count === 0 ? true : false; 
+    let arr = [];
+    for (const item of s) {
+        if (item === '(') {
+            arr.push(item);
+            continue;
+        }
+        
+        if (arr.length === 0) return false;
+        arr.pop();
     }
-    return false;
+    
+    return arr.length === 0 ? true : false; 
 }
