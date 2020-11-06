1b9a3dd100f780aa0dbfc8f351b6e0f732c8289f
조혜선 <jjohye11@gmail.com>
2020-11-06 10:08:36 +0900

    Create programmers-43163.js
    단어 변환 문제풀이
    
    BFS

---
+++ ALGORITHM/js/programmers-43163.js
@@ -0,0 +1,32 @@
+/** 
+ * 단어 변환
+ * https://programmers.co.kr/learn/courses/30/lessons/43163
+ * 
+ * @ddunny
+*/
+function solution(begin, target, words) {
+    if (!words.includes(target)) return 0;
+    let queue = [];
+    let visited = [];
+    const getDiff = (a, b) => a.split("").reduce((cnt, curValue, curIndex) => cnt + (curValue !== b[curIndex]), 0);
+    
+    queue.push(begin);
+    visited[begin] = 0; // 방문처리
+
+    while(queue.length > 0) {
+        let cur = queue.shift();
+
+        if (cur === target) {
+            return visited[cur];
+        }
+        
+        for (const word of words) {
+            if (getDiff(cur, word) === 1 && visited[word] === undefined) {
+                queue.push(word);
+                visited[word] = visited[cur] + 1;
+            }
+        } 
+    }
+    
+    return 0;
+}
