97c6cd43d7cbdf7ffd00a8a45ecca945db083c70
sunny <jjohye11@gmail.com>
2021-06-26 00:22:41 +0900

    프린터프린터

--- ALGORITHM/js/programmers-42587.js
+++ ALGORITHM/js/programmers-42587.js
@@ -1,42 +1,43 @@
-/** 
- * 프린터
- * https://programmers.co.kr/learn/courses/30/lessons/42587
- * - 
- * 
- * @ddunny
-*/
+// 프린터
+// https://programmers.co.kr/learn/courses/30/lessons/42587
 
-function solution(priorities, location) {    
-    let mapping = priorities.map((v, i) => {
-        return {'origin': i, 'value': v};
-    });
-       
-    let target;
-    let print = [];
-    
-    // 대기목록 세팅
-    while (true) {
-        
-        // 대기목록에 대상이 없으면 while문 종료
-        if (mapping.length === 0) break;
-        
-         target = mapping[0]; // 인쇄대상(리스트 가장 앞에 있는 문서)
-        
-        // 대기목록에 target보다 높은 우선순위가 없다면 인쇄
-        if (mapping.find(v => target.value < v.value) === undefined) {
-            print.push(target);
-            mapping.shift();
-            continue;
-        };
-        
-        // 맨 앞에 원소 꺼내서 뒤로 넣기
-        mapping.shift(); 
-        mapping.push(target);
-    }
-    
+function solution(priorities, location) {
+  const list = priorities.map((t, i) => ({
+    my: i === location,
+    val: t,
+  }));
 
-    for (let item in print) {
-        if (print[item].origin === location) 
-            return item * 1 + 1; //i는 index이므로 인쇄순서를 알기 위해서는 +1을 해줘야 함
+  let count = 0;
+  while (1) {
+    const cur = list.shift();
+    if (list.some((t) => t.val > cur.val)) {
+      list.push(cur);
+      continue;
     }
+    count++;
+    if (cur.my) return count;
+  }
 }
+
+/**
+  테스트 1 〉	통과 (0.51ms, 30.2MB)
+  테스트 2 〉	통과 (0.57ms, 30MB)
+  테스트 3 〉	통과 (0.24ms, 30.2MB)
+  테스트 4 〉	통과 (0.22ms, 30.3MB)
+  테스트 5 〉	통과 (0.13ms, 30MB)
+  테스트 6 〉	통과 (0.24ms, 30MB)
+  테스트 7 〉	통과 (0.27ms, 30.3MB)
+  테스트 8 〉	통과 (0.61ms, 30MB)
+  테스트 9 〉	통과 (0.25ms, 29.9MB)
+  테스트 10 〉	통과 (0.31ms, 30.1MB)
+  테스트 11 〉	통과 (0.51ms, 30.2MB)
+  테스트 12 〉	통과 (0.19ms, 30.2MB)
+  테스트 13 〉	통과 (0.52ms, 30MB)
+  테스트 14 〉	통과 (0.13ms, 30.4MB)
+  테스트 15 〉	통과 (0.18ms, 30.1MB)
+  테스트 16 〉	통과 (0.21ms, 30MB)
+  테스트 17 〉	통과 (0.64ms, 30.1MB)
+  테스트 18 〉	통과 (0.18ms, 30.1MB)
+  테스트 19 〉	통과 (0.59ms, 30.2MB)
+  테스트 20 〉	통과 (0.26ms, 30.1MB)
+   */
