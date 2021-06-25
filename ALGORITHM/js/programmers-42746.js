971d377974ce9868f1a565aef443fc27216ded2d
sunny <jjohye11@gmail.com>
2021-06-26 00:17:34 +0900

    가장 큰 수 코드 개선

--- ALGORITHM/js/programmers-42746.js
+++ ALGORITHM/js/programmers-42746.js
@@ -1,34 +1,26 @@
-/**
- * 가장 큰 수
- * https://programmers.co.kr/learn/courses/30/lessons/42746
- *
- * 
- @ddunny
- */
+// 가장 큰 수
+// https://programmers.co.kr/learn/courses/30/lessons/42746
+
 function solution(numbers) {
-  let answer = '';
-  let temp = numbers.slice(0);
-  temp.sort((a, b) => {
-    const charA = a.toString();
-    const charB = b.toString();
-    return parseInt(charB + charA) - parseInt(charA + charB);
-  }); // 첫번째 자리 기준으로 정렬
+  const answer = numbers
+    .map((v) => v + '')
+    .sort((a, b) => (b + a) * 1 - (a + b) * 1)
+    .join('');
 
-  if (temp[0] === 0) {
-    return "0";
-  }
-
-  answer = temp.join(""); // 정답이 너무 클 수 있으니 문자열로 바꾸어 return
-  return answer;
+  // answer[0] === '0': 00000 인 경우 처리를 위해
+  return answer[0] === '0' ? '0' : answer;
 }
 
 /**
- * 다른 사람의 풀이
- * function solution(numbers) {
- *   let temp = numbers.map(v => v + "")
- *     .sort((a, b) => parseInt(b + a) - parseInt(a + b))
- *     .join("");
- * 
- *   return (temp[0] === "0") ? "0" : temp;
-}
+테스트 1 〉	통과 (109.75ms, 39.4MB)
+테스트 2 〉	통과 (64.52ms, 36.9MB)
+테스트 3 〉	통과 (146.28ms, 45.9MB)
+테스트 4 〉	통과 (4.45ms, 32.3MB)
+테스트 5 〉	통과 (129.35ms, 37.9MB)
+테스트 6 〉	통과 (92.01ms, 38.4MB)
+테스트 7 〉	통과 (0.10ms, 30.1MB)
+테스트 8 〉	통과 (0.07ms, 30MB)
+테스트 9 〉	통과 (0.10ms, 30.2MB)
+테스트 10 〉	통과 (0.06ms, 30MB)
+테스트 11 〉	통과 (0.09ms, 30MB)
  */
